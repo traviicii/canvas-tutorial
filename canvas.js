73e0167100e9f315e9b2bdd2df25804745e4f88f
traviicii <125103256+traviicii@users.noreply.github.com>
2023-09-14 21:36:37 -0400

    animating circles on the canvas

--- canvas.js
+++ canvas.js
@@ -38,34 +38,66 @@ var c = canvas.getContext('2d');
 // c.stroke();
 // }
 
-c.beginPath();
-c.arc(200, 200, 30, 0, Math.PI * 2, false);
-c.strokeStyle = 'blue'
-c.stroke();
+// c.beginPath();
+// c.arc(200, 200, 30, 0, Math.PI * 2, false);
+// c.strokeStyle = 'blue'
+// c.stroke();
 
-var x = 200;
-var y = 200;
-var dx = 4;
-var dy = 4;
-var radius = 30;
+
+function Circle(x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+
+    this.draw = function () {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.strokeStyle = 'blue';
+        c.stroke();
+        c.fill()
+    }
+
+    this.update = function () {
+        if (this.x + this.radius > innerWidth || 
+            this.x - this.radius < 0) {
+            this.dx = -this.dx;
+        }
+
+        if (this.y + this.radius > innerHeight || 
+            this.y - this.radius < 0) {
+            this.dy = -this.dy;
+        }
+
+        this.x += this.dx;
+        this.y += this.dy;
+
+        this.draw();
+    }
+}
+
+
+var circleArray = [];
+
+for (var i = 0; i < 100; i++) {
+    var radius = 80;
+    var x = Math.random() * (innerWidth - radius * 2) + radius
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
+    var dx = (Math.random() - 0.5);
+    var dy = (Math.random() - 0.5);
+    circleArray.push(new Circle(x, y, dx, dy, radius));    
+}
+
+
+// circle.draw()
+
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
 
-    c.beginPath();
-    c.arc(x, y, radius, 0, Math.PI * 2, false);
-    c.strokeStyle = 'blue';
-    c.stroke();
-
-    if (x + radius > innerWidth || x - radius < 0) {
-        dx = -dx;
+    for (var i = 0; i < circleArray.length; i++) {
+        circleArray[i].update();
     }
-
-    if (y + radius > innerHeight || y - radius < 0) {
-        dy = -dy;
-    }
-
-    x += dx;
-    y += dy;
 }
 animate()
